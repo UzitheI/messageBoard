34d282ecf82238816c858da08d1aa791bb79e8cc
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-08 21:09:38 +0545

    changes after ejs

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/new');
+var usersRouter = require('./routes/users');
+var newRouter = require('./routes/new')
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/new', usersRouter);
+app.use('/users', usersRouter);
+app.use('/new',newRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
